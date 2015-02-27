4f7a1bae4dbd29c09135474326993ae3c39b460a
rachitpandya <pandyarachit007@gmail.com>
2015-02-27 13:14:32 -0500

    Final Coding

--- Assignment2JqueryMobile/Scripts/_references.js
+++ Assignment2JqueryMobile/Scripts/_references.js
@@ -2,3 +2,5 @@
 /// <reference path="jquery-1.8.0.js" />
 /// <reference path="jquery.mobile-1.4.5.js" />
 /// <reference path="mobile.js" />
+/// <reference path="jssor.slider.min.js" />
+/// <reference path="jquery-1.9.1.min.js" />
